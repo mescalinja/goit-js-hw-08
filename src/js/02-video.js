418661d9860a114613de3934b9da5c452a96ee86
mescalinja <a.semyonova@icloud.com>
2022-05-01 15:00:54 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Vimeo.Player('vimeo-player');
+
+const KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle((data) =>
+    localStorage.setItem(KEY, data.seconds), 1000));
+
+const savedTime = localStorage.getItem(KEY);
+
+if (savedTime) {
+    player.setCurrentTime(savedTime)
+}
+
 
