bffbd9e609911261fe6b8837f7daea85cbba14c7
mescalinja <a.semyonova@icloud.com>
2022-05-01 15:41:54 +0300

    third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const inputEmail = document.querySelector("[name='email']")
+const inputTxt = document.querySelector("[name='message'")
+
+const KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));
+};
+
+function onPageReload() {
+    const savedData = JSON.parse(localStorage.getItem(KEY));
+
+    if (savedData) {
+        inputEmail.value = savedData.email;
+        inputTxt.value = savedData.message;
+
+        formData.email = savedData.email;
+        formData.message = savedData.message;
+    }
+};
+
+onPageReload();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    localStorage.removeItem(KEY);
+    e.currentTarget.reset();
+
+    console.log(formData)
+
+    formData.email = '';
+    formData.message = '';
+};
 
